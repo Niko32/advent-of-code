400c4354b57524400e107f84ee07454e9fb13b94
Niko <niko.konzack@t-online.de>
2023-12-09 17:39:29 +0100

    day 5.5

---
+++ day-06/src/main.rs
@@ -0,0 +1,46 @@
+mod structs;
+use crate::structs::*;
+
+pub const EXAMPLE_INPUT: &str = include_str!("../data/example_input.txt");
+
+pub fn solve_puzzle1(input: &str) -> i64 {
+    let races = Races::from(input);
+    races
+        .vec
+        .into_iter()
+        .map(|race| race.n_winning_strategies())
+        .product::<usize>() as i64
+}
+
+pub fn solve_puzzle2(input: &str) -> i64 {
+    todo!()
+}
+
+fn main() {
+    const INPUT: &str = include_str!("../data/input1.txt");
+
+    let solution1 = solve_puzzle1(INPUT);
+    println!("Solution to puzzle one: {}", solution1);
+
+    let solution2 = solve_puzzle2(INPUT);
+    println!("Solution to puzzle two: {}", solution2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_input1() {
+        const EXAMPLE_OUTPUT: i64 = 288;
+        let result = solve_puzzle1(EXAMPLE_INPUT);
+        assert_eq!(result, EXAMPLE_OUTPUT);
+    }
+
+    #[test]
+    fn test_example_input2() {
+        const EXAMPLE_OUTPUT: i64 = 46;
+        let result = solve_puzzle2(EXAMPLE_INPUT);
+        assert_eq!(result, EXAMPLE_OUTPUT);
+    }
+}
---
+++ day-06/src/structs.rs
@@ -0,0 +1,67 @@
+pub struct Races {
+    pub vec: Vec<Race>,
+}
+
+pub struct Race {
+    pub allowed_time: u64,
+    pub record_distance: u64,
+}
+
+impl From<&str> for Races {
+    fn from(value: &str) -> Self {
+        let mut split = value.lines();
+        let time_str = split.next().expect("Split should have one line for time");
+        let distance_str = split
+            .next()
+            .expect("Split should have a second line for distance");
+
+        let times = time_str
+            .split(':')
+            .nth(1)
+            .expect("Time string should contain times after colon")
+            .split_ascii_whitespace()
+            .map(|time_str| {
+                time_str
+                    .parse::<u64>()
+                    .expect("Time substrings should be parsable as u64")
+            });
+        let distances = distance_str
+            .split(':')
+            .nth(1)
+            .expect("Distance string should contain times after colon")
+            .split_ascii_whitespace()
+            .map(|s| {
+                s.parse::<u64>()
+                    .expect("Distance substrings should be parsable as u64")
+            });
+
+        Races {
+            vec: times
+                .zip(distances)
+                .into_iter()
+                .map(|tuple| Race {
+                    allowed_time: tuple.0,
+                    record_distance: tuple.1,
+                })
+                .collect(),
+        }
+    }
+}
+
+impl Race {
+    pub fn strategies(&self) -> Vec<u64> {
+        let mut strategies = Vec::with_capacity(self.allowed_time as usize + 1);
+        for charge_time in 0..=self.allowed_time {
+            let distance = (self.allowed_time - charge_time) * charge_time;
+            strategies.push(distance);
+        }
+        strategies
+    }
+
+    pub fn n_winning_strategies(&self) -> usize {
+        self.strategies()
+            .into_iter()
+            .filter(|&strategy| strategy > self.record_distance)
+            .count()
+    }
+}
