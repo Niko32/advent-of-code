eec078766754fc0af39367d00cb88b8ca353c2cc
Niko <niko.konzack@airealgroup.com>
2025-12-03 10:32:04 +0100

    2025 Day 2 Puzzle 1

--- 2025/day-02/src/main.rs
+++ 2025/day-02/src/main.rs
@@ -2,18 +2,46 @@ mod structs;
 
 pub const EXAMPLE_INPUT: &str = include_str!("../data/example_input.txt");
 
-fn ranges(s: &str) Vec<(i32, i32)> {
-    s.split(",")
-        .map(|substring| { substring.split("-") as (i32, i32)})
+fn parse_ranges(s: &str) -> Vec<(&str, &str)> {
+    s.trim_end()
+        .split(",")
+        .map(|substring| {
+            substring.split_once("-")
+                .expect("range has more than one - delimiter")
+        })
+        .collect()
 }
 
-fn solve_puzzle1(input: &str) -> i32 {
-    
+fn is_invalid(s: &str) -> bool {
+    if s.len() % 2 != 0 {
+        return false
+    }
+
+    let (a, b) = s.split_at((s.len() / 2) as usize);
+    return a == b
 }
 
-fn solve_puzzle2(input: &str) -> i32 {
-    
+fn solve_puzzle1(input: &str) -> i64 {
+    let mut sum = 0;
+
+    for (start, end) in parse_ranges(input) {
+        println!("{:?}, {:?}", start, end);
+        let start_int: usize = start.parse().expect("failed to parse start to int");
+        let end_int: usize = end.parse().expect("failed to parse end to int");
+
+        for i in start_int..=end_int {
+            if is_invalid(&i.to_string()) {
+                sum += i
+            }
+        }
+    }
+
+    return sum as i64
 }
+
+// fn solve_puzzle2(input: &str) -> i64 {
+//     return 0
+// }
 
 fn main() {
     const INPUT: &str = include_str!("../data/input1.txt");
@@ -21,8 +49,8 @@ fn main() {
     let solution1 = solve_puzzle1(INPUT);
     println!("Solution to puzzle one: {}", solution1);
 
-    let solution2 = solve_puzzle2(INPUT);
-    println!("Solution to puzzle two: {}", solution2);
+    // let solution2 = solve_puzzle2(INPUT);
+    // println!("Solution to puzzle two: {}", solution2);
 }
 
 #[cfg(test)]
@@ -31,15 +59,15 @@ mod tests {
 
     #[test]
     fn test_example_input1() {
-        const EXAMPLE_OUTPUT: i32 = 3;
+        const EXAMPLE_OUTPUT: i64 = 1227775554;
         let result = solve_puzzle1(EXAMPLE_INPUT);
         assert_eq!(result, EXAMPLE_OUTPUT);
     }
 
-    #[test]
-    fn test_example_input2() {
-        const EXAMPLE_OUTPUT: i32 = 6;
-        let result = solve_puzzle2(EXAMPLE_INPUT);
-        assert_eq!(result, EXAMPLE_OUTPUT);
-    }
+    // #[test]
+    // fn test_example_input2() {
+    //     const EXAMPLE_OUTPUT: i64 = 6;
+    //     let result = solve_puzzle2(EXAMPLE_INPUT);
+    //     assert_eq!(result, EXAMPLE_OUTPUT);
+    // }
 }
