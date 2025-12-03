fb8c1d7efa7a5ba2a15fe6bdb334e5c1fcd1960a
Niko <niko.konzack@airealgroup.com>
2025-12-03 15:55:31 +0100

    Add time measurements

--- 2025/day-04/src/main.rs
+++ 2025/day-04/src/main.rs
@@ -1,28 +1,44 @@
 mod structs;
 
-pub const EXAMPLE_INPUT: &str = include_str!("../data/example_input.txt");
+use std::fs::File;
+use std::io::Write;
+use std::time::Instant;
 
-fn solve_puzzle1(input: &str) -> i32 {
-    
-}
+fn parse_input(input: &str) -> &str { "" }
 
-fn solve_puzzle2(input: &str) -> i32 {
-    
-}
+fn solve_puzzle1(parsed_input: &str) -> i32 { 0 }
+
+fn solve_puzzle2(parsed_input: &str) -> i32 { 0 }
 
 fn main() {
     const INPUT: &str = include_str!("../data/input1.txt");
 
-    let solution1 = solve_puzzle1(INPUT);
+    let mut file = File::create("../times.txt")
+        .expect("failed to create file handle");
+
+    let parse_timer = Instant::now();
+    let parsed_input = parse_input(INPUT);
+    let parse_time = parse_timer.elapsed();
+
+    let timer1 = Instant::now();
+    let solution1 = solve_puzzle1(parsed_input);
+    let time1 = timer1.elapsed();
     println!("Solution to puzzle one: {}", solution1);
 
-    let solution2 = solve_puzzle2(INPUT);
+    let timer2 = Instant::now();
+    let solution2 = solve_puzzle2(parsed_input);
+    let time2 = timer2.elapsed();
     println!("Solution to puzzle two: {}", solution2);
+
+    writeln!(file, "time for parsing:    {:?}\ntime for puzzle one: {:?}\ntime for puzzle two: {:?}", parse_time, time1, time2)
+        .expect("failed to write to times.txt");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const EXAMPLE_INPUT: &str = include_str!("../data/example_input.txt");
 
     #[test]
     fn test_example_input1() {
