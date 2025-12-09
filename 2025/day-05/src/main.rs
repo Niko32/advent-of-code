4b2940a0ff8b4bbdac0b57f4790fd9e4651df3d7
Niko <niko.konzack@airealgroup.com>
2025-12-09 09:46:54 +0100

    2025 Day 5 Puzzle 2

--- 2025/day-05/src/main.rs
+++ 2025/day-05/src/main.rs
@@ -3,10 +3,9 @@ mod structs;
 use std::fs::File;
 use std::io::Write;
 use std::time::Instant;
-use std::cell::UnsafeCell;
-use rayon::prelude::*;
+// use rayon::prelude::*;
 
-fn parse_input(input: &str) -> (Vec<[u32; 2]>, Vec<u32>) { 
+fn parse_input(input: &str) -> (Vec<[u64; 2]>, Vec<u64>) { 
     let parts: Vec<&str> = if input.contains("\r\n\r\n") {
         input.trim_end().split("\r\n\r\n").collect()
     } else {
@@ -14,30 +13,30 @@ fn parse_input(input: &str) -> (Vec<[u32; 2]>, Vec<u32>) {
     };
     let (range_lines, id_lines) = (parts[0], parts[1]);
 
-    let ranges: Vec<[u32; 2]> = range_lines
+    let ranges: Vec<[u64; 2]> = range_lines
         .lines()
         .map(|line| {
-            let nums: Vec<u32> = line
+            let nums: Vec<u64> = line
                 .split("-")
                 .map(|line| {
-                    line.parse::<u32>().expect("failed to parse ranges as u32")
+                    line.parse::<u64>().expect("failed to parse ranges as u64")
                 })
                 .collect();
             [nums[0], nums[1]]
         })
         .collect();
 
-    let ids: Vec<u32> = id_lines
+    let ids: Vec<u64> = id_lines
         .lines()
         .map(|line| {
-            line.parse().expect("failed to parse id as u32")
+            line.parse().expect("failed to parse id as u64")
         })
         .collect();
 
     return (ranges, ids)
 }
 
-fn solve_puzzle1(ranges: &Vec<[u32; 2]>, ids: &Vec<u32>) -> u32 {
+fn solve_puzzle1(ranges: &Vec<[u64; 2]>, ids: &Vec<u64>) -> u64 {
     let mut fresh_counter = 0;
     for id in ids {
         for range in ranges{
@@ -50,17 +49,38 @@ fn solve_puzzle1(ranges: &Vec<[u32; 2]>, ids: &Vec<u32>) -> u32 {
     fresh_counter
 }
 
-fn solve_puzzle2(ranges: &Vec<[u32; 2]>, _: &Vec<u32>) -> u32 {
-    let mut fresh_ids = UnsafeCell::new([false; u32::MAX as usize]);
-    ranges.par_iter().for_each(|range| {
-        unsafe {
-            let ptr = fresh_ids.get();
-            for i in range[0]..=range[1] {
-                (*ptr)[i as usize] = true;
+fn solve_puzzle2(ranges: &Vec<[u64; 2]>, _: &Vec<u64>) -> u64 {
+    let mut ranges2 = ranges.clone();
+    let mut i: usize = 0;
+    let mut full_iteration: bool = true;
+
+    loop { // Merge ranges together
+        // println!("i: {}, ranges2.len(): {}", i, ranges2.len());
+        if i+1 >= ranges2.len() {
+            break
+        }
+        for j in i+1..ranges2.len() {
+            // println!("  j: {}, ranges2: {:?}", j, ranges2);
+            full_iteration = true;
+            if ranges2[i][0] <= ranges2[j][1] && ranges2[i][1] >= ranges2[j][0] {
+                ranges2[i][0] = ranges2[i][0].min(ranges2[j][0]);
+                ranges2[i][1] = ranges2[i][1].max(ranges2[j][1]);
+                ranges2.remove(j);
+                full_iteration = false;
+                break
             }
         }
-    });
-    fresh_ids.into_inner().iter().filter(|x|**x).count() as u32
+        if full_iteration {
+            i += 1;
+        }
+    }
+
+    let mut fresh_count = 0;
+    for range in ranges2 {
+        fresh_count += 1 + range[1] - range[0];
+    }
+
+    fresh_count
 }
 
 fn main() {
@@ -95,7 +115,7 @@ mod tests {
 
     #[test]
     fn test_example_input1() {
-        const EXAMPLE_OUTPUT: u32 = 3;
+        const EXAMPLE_OUTPUT: u64 = 3;
         let (ranges, ids) = parse_input(EXAMPLE_INPUT);
         let result = solve_puzzle1(&ranges, &ids);
         assert_eq!(result, EXAMPLE_OUTPUT);
@@ -103,7 +123,7 @@ mod tests {
 
     #[test]
     fn test_example_input2() {
-        const EXAMPLE_OUTPUT: u32 = 14;
+        const EXAMPLE_OUTPUT: u64 = 14;
         let (ranges, ids) = parse_input(EXAMPLE_INPUT);
         let result = solve_puzzle2(&ranges, &ids);
         assert_eq!(result, EXAMPLE_OUTPUT);
